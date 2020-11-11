54703d84f7893cebfaf3d9a948e60ab3ad5c0405
yametai2kin <72928198+yametai2kin@users.noreply.github.com>
2020-11-12 03:33:18 +0900

    コミット

---
+++ Assets/BallManager.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallManager : MonoBehaviour
+{
+    public GameObject templateBall = null;
+    public Slider sliderPower = null;
+    public Slider sliderMaxTorque = null;
+    private GameObject currentBall = null;
+    private bool isShootKey = false;
+    private float shootTimer = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.templateBall.SetActive( false );
+        CreateBall();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // マウスクリックを見張る
+        if( Input.GetMouseButtonDown( 0 ) )
+        {
+            this.isShootKey = true;
+        }
+
+        // 一定時間経過でボールを複製
+        if( this.currentBall == null )
+        {
+            this.shootTimer += Time.deltaTime;
+            if( this.shootTimer > 1.5f )
+            {
+                CreateBall();
+                this.shootTimer = 0;
+            }
+        }
+    }
+
+    /*
+     * 剛体へ力を加える場合はFixedUpdateで行う方が処理速度による結果の誤差が少ない
+     */
+    void FixedUpdate()
+    {
+        if( this.isShootKey )
+        {
+            Shoot();
+            this.isShootKey = false;
+        }
+    }
+
+    /*
+     * ボールを打ち出す
+     */
+    void Shoot()
+    {
+        if( this.currentBall == null )
+        {
+            return;
+        }
+
+        // 現在のボールをクリックしたか？
+        Ray ray = Camera.main.ScreenPointToRay( Input.mousePosition );
+        Debug.DrawRay( ray.origin, ray.direction, Color.red, 100 );
+        RaycastHit hit;
+        if( !Physics.Raycast( ray, out hit, Mathf.Infinity ) )
+        {
+            return;
+        }
+        if( hit.transform != this.currentBall.transform )
+        {
+            return;
+        }
+
+        // クリック地点に対して水平に力を加える(跳ねないように)
+        Rigidbody rigidbody = this.currentBall.GetComponent<Rigidbody>();
+        //rigidbody.AddForceAtPosition( Vector3.forward * sliderPower.value, hit.point, ForceMode.Acceleration );
+        rigidbody.AddForceAtPosition( ( hit.point - Camera.main.transform.position ).normalized * sliderPower.value, hit.point, ForceMode.Acceleration );
+        rigidbody.constraints = rigidbody.constraints & ~RigidbodyConstraints.FreezeAll;
+        this.currentBall = null;
+    }
+
+    /*
+     * ボールを複製
+     */
+    void CreateBall()
+    {
+        this.currentBall = Instantiate( this.templateBall );
+        this.currentBall.SetActive( true );
+        Rigidbody rigidbody = this.currentBall.GetComponent<Rigidbody>();
+        rigidbody.maxAngularVelocity = sliderMaxTorque.value;
+        rigidbody.constraints = RigidbodyConstraints.FreezePositionY;
+
+    }
+}
---
+++ Assets/FreeCameraController.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreeCameraController : MonoBehaviour
+{
+    private Vector3 mouseRotatePos = new Vector3();
+    private Vector3 mouseMovePos = new Vector3();
+
+    private float mouseAngleX = 0;
+    private float mouseAngleY = 0;
+
+    public float mouseRotateBias = 100;
+    public float mouseMoveBias = 10;
+
+    private Vector3 mousePos = new Vector3();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // マウス操作によるカメラの左右回転
+        UpdateMouseRotate();
+        UpdateMouseMove();
+        UpdateMouseWheel();
+    }
+
+    void UpdateMouseRotate()
+    {
+        if( Input.GetMouseButtonDown( 1 ) )
+        {
+            this.mouseRotatePos = Input.mousePosition;
+            this.mouseAngleX = this.gameObject.transform.eulerAngles.x;
+            this.mouseAngleY = this.gameObject.transform.eulerAngles.y;
+        }
+
+        if( Input.GetMouseButton( 1 ) )
+        {
+            //(移動開始座標 - マウスの現在座標) / 解像度 で正規化
+            float x = ( this.mouseRotatePos.x - Input.mousePosition.x ) / Screen.width;
+            float y = ( this.mouseRotatePos.y - Input.mousePosition.y ) / Screen.height;
+
+            //回転開始角度 ＋ マウスの変化量 * マウス感度
+            float eulerX = this.mouseAngleX + y * this.mouseRotateBias;
+            float eulerY = this.mouseAngleY + x * this.mouseRotateBias;
+
+            this.gameObject.transform.rotation = Quaternion.Euler( eulerX, eulerY, 0 );
+        }
+    }
+
+    void UpdateMouseMove()
+    {
+        if( Input.GetMouseButtonDown( 2 ) )
+        {
+            this.mouseMovePos = Input.mousePosition;
+            this.mousePos = this.gameObject.transform.position;
+        }
+
+        if( Input.GetMouseButton( 2 ) )
+        {
+            //(移動開始座標 - マウスの現在座標) / 解像度 で正規化
+            Vector3 vec = new Vector3();
+            vec.x = ( this.mouseMovePos.x - Input.mousePosition.x ) / Screen.width;
+            vec.y = ( this.mouseMovePos.y - Input.mousePosition.y ) / Screen.height;
+
+            //回転開始角度 ＋ マウスの変化量 * マウス感度
+            vec = this.gameObject.transform.rotation * vec * this.mouseMoveBias;
+            this.gameObject.transform.position = this.mousePos + vec;
+        }
+    }
+
+    void UpdateMouseWheel()
+    {
+        float scroll = Input.mouseScrollDelta.y;
+        if( scroll == 0 )
+        {
+            return;
+        }
+
+           //(移動開始座標 - マウスの現在座標) / 解像度 で正規化
+            Vector3 vec = new Vector3( 0, 0, scroll );
+            vec = this.gameObject.transform.rotation * vec;
+            this.gameObject.transform.position += vec;
+    }
+
+}
